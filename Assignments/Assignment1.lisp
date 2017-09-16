38fd7cdcb760f3a13733d4a399ee41b67f665532
Kanat Alimanov <kanat.alimanov@nu.edu.kz>
2017-09-16 19:43:55 +0600

    Assignment 1 Recursive BFS/DFS Implemented

---
+++ Assignments/Assignment1.lisp
@@ -0,0 +1,146 @@
+; tree printing dfs functions
+(defun print_children (tree)
+	(cond 	
+		((not (null tree)) 
+			(format t " ~A " (car (car tree)))
+			(print_children (cdr tree))
+		)
+		(t (format t "|~%"))
+	)
+)
+(defun use_children (tree)
+	(cond
+		((not (null tree))
+			(print_tree (car tree))
+			(use_children (cdr tree))
+		))
+)
+(defun print_tree (tree) 
+	(cond 	
+		((not (null tree)) 
+			(format t "> ~A" (car tree))
+			(format t " --> |")
+			(print_children (cdr tree))
+			(use_children (cdr tree))
+		))
+)
+; tree printing dfs end
+
+; tree printing bfs functions
+(defun trim_first (tree)
+	(cond 
+		((not (null (cdr (car tree)))) 
+			(cons (remove_parentheses (cdr (car tree))) (trim_mod (cdr tree)))
+		)
+		((not (null (cdr tree)))
+			(trim_mod (cdr tree))
+		)
+		(t 
+			nil
+		)
+	)
+)
+(defun trim_mod(tree)
+	(cond 
+		((listp (car (remove_parentheses tree)))
+			(trim_first (remove_parentheses tree))
+		)
+		(t (trim_first tree))
+	)
+	;(trim_first (remove_parentheses tree))
+)
+(defun remove_parentheses (tree)
+	(cond
+		(( not (atom (car tree)))
+			(cond 
+				((not (<= (list-length tree) (list-length (car tree)))) 
+					tree
+				)
+				(t (remove_parentheses (car tree)))
+			)
+		)
+		(t 
+			tree
+		)
+	)
+)
+(defun print_tree_bfs (tree)
+	(format t "~A --> |" (car tree))
+	(print_children (cdr tree))
+	(format t "going deeper~%")
+	(bfs_print (cdr tree))
+)
+(defun bfs_print (nodes)
+	(cond
+		((not (null nodes))
+			(children_printer_helper nodes)
+			(format t "going deeper~%")
+			(bfs_print (trim_mod nodes))
+		)
+	)
+)
+(defun children_printer_helper (nodes)
+	(cond 
+		((not (null (car nodes)))
+			(cond 
+				((listp (car (car nodes)))
+					(children_printer_helper (car nodes))
+					(children_printer_helper (cdr nodes))
+				)
+				(t 
+					(format t "~A --> |" (car (car nodes)))
+					(print_children (cdr (car nodes)))
+					(children_printer_helper (cdr nodes))
+				)
+			)
+		)
+	)
+)
+; tree printing bfs end
+
+;tree searching functions
+(defun search_tree (tree start switch)
+	(print tree)
+	(print start)
+	(print switch)
+)
+(defun find_subtree (tree node)
+	(cond 
+		((not (null tree))
+			(find_subtree (car tree) node)
+			(find_subtree (cdr tree) node)
+		))
+)
+(defun print_children_f (tree node)
+	(cond 	
+		((eq (car tree) node)
+			(print tree)
+		)
+		((not (null tree)) 
+			(print_children_f (cdr tree) node)
+		)
+	)
+)
+(defun use_children_f (tree node)
+	(cond
+		((not (null tree))
+			(cond )
+			(print_tree_f (car tree) node)
+			(use_children_f (cdr tree) node)
+		))
+)
+(defun print_tree_f (tree node) 
+	(cond 	
+		((not (null tree)) 
+			(cond 
+				((eq (car tree) node) 
+					(print tree)
+				)
+				(t 
+					(print_children_f (cdr tree) node)
+					(use_children_f (cdr tree) node)
+				)
+			)
+		))
+)
+;tree searching functions end
